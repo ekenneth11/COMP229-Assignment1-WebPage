87d0b981a1529bf516f83a3792ab10df77ffe8d7
Kenneth Bautista <kennethbautista1512@gmail.com>
2026-02-02 16:25:07 -0500

    added the route and the assets

--- webpage/src/App.jsx
+++ webpage/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router} from 'react-router-dom'
+import MainRouter from './MainRouter'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      hello
+      <Router>
+        <MainRouter />
+      </Router>
     </>
   )
 }
---
+++ webpage/src/MainRouter.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
+
+function MainRouter(){
+    return(
+        <>
+            <Routes>
+                
+            </Routes>
+        </>
+    )
+}
+
+export default MainRouter;
