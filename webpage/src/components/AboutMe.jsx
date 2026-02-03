73ea051bea262316c47f0c50a7cb6b041caf0ab1
Kenneth Bautista <kennethbautista1512@gmail.com>
2026-02-02 19:50:56 -0500

    finished the about me page

--- webpage/src/components/AboutMe.jsx
+++ webpage/src/components/AboutMe.jsx
@@ -1,9 +1,28 @@
 import Header from "./Header";
-
+import me from '../assets/me.jpg'
+import './aboutme.css'
+import resume from '../assets/KennethBautistaResume.pdf'
 function AboutMe(){
     return(
         <>
             <Header />
+            <h1 className="title">I'm Kenneth.</h1>
+            <div className="aboutMe-content">
+                <div>
+                    <img src={me} alt="Me" className="meImage" />
+                </div>
+                
+                <div className="description"> 
+                    <p> I am a Computer Science graduate specializing in Health Informatics. 
+                    My goal is to bridge the gap between complex technology and patient care 
+                    by building efficient, secure, and user-friendly systems. I am currently 
+                    seeking opportunities to apply my skills in full-stack development and data analysis.
+                    </p>
+                    <a href={resume} target="_blank" className="download-button">Download Resume <span classname="arrow">↓</span></a>
+                </div>
+
+            </div>
+            
         </>
     )
 
--- webpage/src/components/Home.jsx
+++ webpage/src/components/Home.jsx
@@ -1,13 +1,14 @@
 import Header from "./Header";
-
+import './home.css'
 function Home(){
     return(
         <>
             <Header/>
             <div className="homepage">
-                <h1>Hi. I'm Kenneth.<br></br>Welcome to my Portfolio</h1>
-                <p>Combining technical precision with creative design. My goal is to build digital 
-                    experiences that are not only functional and data-driven but also intuitive and visually engaging.</p>
+                <h1>Hi. I'm Kenneth. <br></br>Welcome to my Portfolio</h1>
+                <p>Combining technical precision with creative design. 
+                    My goal is to build digital experiences that are not only functional 
+                    and data-driven but also intuitive and visually engaging.</p>
             </div>
         </>
     )
