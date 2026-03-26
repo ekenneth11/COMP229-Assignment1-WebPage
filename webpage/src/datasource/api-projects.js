22c2f4e517ba4d057e4baaa7caa1a0a1428b061d
Kenneth Bautista <kennethbautista1512@gmail.com>
2026-03-26 14:55:11 -0400

    added the api

---
+++ webpage/src/datasource/api-projects.js
@@ -0,0 +1,85 @@
+let apiURL = import.meta.env.VITE_APP_APIURL;
+let endpoint = "/api/projects/";
+
+
+//listing all the document from the database
+const list = async() => {
+    try{
+        let response = await fetch(apiURL + endpoint, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        return await response.json();
+    }catch(error){
+        console.log(error);
+    }
+}
+
+//adding a new document to the database
+const create = async (item) => {
+    try{
+        let response = await fetch(apiURL+ endpoint,{
+            method: 'POST',
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item)
+        });
+        return await response.json();
+    }catch(error){
+        console.log(error);
+    }
+}
+
+//updating a document in the database
+const update = async (item, id) =>{
+    try{
+        let response = await fetch(apiURL + endpoint + id, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item)
+        });
+        return await response.json();
+    }catch(error){
+        console.log(error);
+    }
+}
+
+//removing a document from the database
+const remove = async (item, id) => {
+    try{
+        let response = await fetch(apiURL + endpoint + id, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        return await response.json();
+    }catch(error){
+        console.log(error);
+    }
+}
+
+//getting one document from the database
+const getOne = async (item, id) => {
+    try{
+        let response = await fetch(apiURL + endpoint + id, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        return await response.json();
+    }catch(error){
+        console.log(error);
+    }
+}
